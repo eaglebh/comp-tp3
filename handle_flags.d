337c303d76ffd9a55a84ace993233c290b246ef5
Pablo Marcondes Fonseca <pablo.mmarcondes@gmail.com>
2014-05-25 09:25:58 -0300

    deletando arquivos gerados

--- handle_flags.d
+++
@@ -1,5 +0,0 @@
-handle_flags.o handle_flags.d : handle_flags.cc \
- /home/prof/renato/cool/student/include/PA4/copyright.h \
- /home/prof/renato/cool/student/include/PA4/cool-io.h \
- /home/prof/renato/cool/student/include/PA4/copyright.h \
- /home/prof/renato/cool/student/include/PA4/cgen_gc.h
--- tree.d
+++
@@ -1,6 +0,0 @@
-tree.o tree.d : tree.cc /home/prof/renato/cool/student/include/PA4/copyright.h \
- /home/prof/renato/cool/student/include/PA4/tree.h \
- /home/prof/renato/cool/student/include/PA4/copyright.h \
- /home/prof/renato/cool/student/include/PA4/stringtab.h \
- /home/prof/renato/cool/student/include/PA4/list.h \
- /home/prof/renato/cool/student/include/PA4/cool-io.h
